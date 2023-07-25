1631ef1d4bc73ee5ae170b1386497046c221c66b
JacksonKupris <jacksonkupris@gmail.com>
2023-07-24 20:24:34 -0500

    FEAT: Updated to async calls, and made all components functional instead of class. Added handlers for if the pokemon has a type 2 or not. Minor changes to CSS

--- src/App.js
+++ src/App.js
@@ -1,272 +1,94 @@
-// /* 
-// TODO: Add a loading screen for while it is loading the next API get
-// TODO: Do better DRY so that the component did mount is not repeated down below
-// TODO: Change to a functional component, use useEffect and make the api call a function before the return happens.
-// TODO: Change the fetch to be a async fetch so that it fetches things faster
-// TODO:  Need to add to the state the new pokemon that are being loaded with fetchAPI(). Currently it is overwriting the previous state
-// */
-
+// TODO: When you click on a card, bring up more details about the pokemon
 import { useEffect, useState } from "react";
 import "./App.css";
-// import React, { Component } from "react";
 import CardList from "./components/card-list/card-list.component";
 import Pokedex_logo from "./images/Pokedex_logo.png";
-import { Details } from "@mui/icons-material";
+
 const App = () => {
-  const [pokemonArray, setPokemonArray] = useState([])
-  const [detailUrl, setDetailUrl] = useState([])
-  const [pokeDetails, setPokeDetails] = useState([])
-  const [nextUrl, setNextUrl] = useState('')
-  const [prevUrl, setPrevUrl] = useState ('')
-  const [scroll, setScroll] = useState (false)
-const url = "https://pokeapi.co/api/v2/pokemon?limit=100"
+  const [pokemonArray, setPokemonArray] = useState([]);
+  const [pokeDetails, setPokeDetails] = useState([]);
+  const [nextUrl, setNextUrl] = useState("");
+  const [loading, setLoading] = useState(true);
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=27";
 
-  useEffect( () => {
-    
-  getPokemons(url)
-  // fetchAPI(url)
-  //   console.log('useEffect url', url)
+  useEffect(() => {
+    const urlCheck = nextUrl && nextUrl.length ? nextUrl : url;
+
+    getPokemons(urlCheck);
+  }, [loading]);
+
+  window.onscroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      setLoading(true);
+    }
+  };
+  const sort = async (arr) => {
+    try {
+        let i, j, temp;
+
+        for (i = 0; i < arr.length; i++) {
+            for (j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
   
-}, []);
-
-
-
-async function getPokemons(url){
-  // console.log('inside getPokemons function')
-  // console.log('inside getPokemons function url', url)
-  const response = await fetch(url);
-  const data = await response.json();
-  setPokemonArray(data.results);
-  setNextUrl(data.next)
-  setPrevUrl(data.prev)
-  data.results.map(async name => {
-  setDetailUrl(prevDetailUrl => [...prevDetailUrl, name.url])
-})
-  return data
-}
-// const pokemonDAO = new Promise ((resolve, reject) => {
-//   const poke2 = await getPokemons(url)
-//   resolve(getPokemons(url))
-// })
-// console.log(getPokemons())
-// pokemonDAO.then(getDetailsHandler())
-
-
-  // console.log('detailURL', detailUrl)
-  // getDetailsHandler()
+                    temp = await arr[i];
+                    arr[i] = await arr[j];
+                    arr[j] = await temp;
+                }
+            }
+        }
   
-//   function getDetailsHandler(){detailUrl.map(async name => {
-//     console.log('getDetailsHandler name', name)
-//     // getDetails(name)
-//     const pokemons = getDetails(name);
-//     console.log('pokemons', pokemons)
-//     // for (const pokemon of pokemons) {
-//       pokemons.data = await fetch(name).then((res) => res.json());
-//     // }
-//     setPokeDetails(
-// { pokemon: pokemons });
-     
-// })};
-// console.log(detailUrl)
-
-
-
-const handleNext = () => {
-  getPokemons(nextUrl)
+        // Returns the sorted array
+        return arr;
+    }
+    catch (e) {
+  
+        console.error(e)
+    }
 }
-const handlePrevious = () => {
-  getPokemons(prevUrl)
-}
-  return(
-    // <div>placeholder</div>
+  async function getPokemons(url) {
+    const baseUrl = url;
+    try {
+      fetch(baseUrl)
+        .then((response) => {
+          const responseJson = response.json();
+
+          return responseJson;
+        })
+        .then(async (data) => {
+          setNextUrl(data.next);
+          const pokemons = data.results;
+          const sortedPokemon = await sort(pokemons)
+          setPokemonArray(sortedPokemon);
+        })
+        .then(() => {
+          pokemonArray.map((el) => getDetails(el));
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function getDetails(pokemon) {
+    pokemon.data = await fetch(pokemon.url).then((res) => res.json());
+    setPokeDetails((prevState) => [...prevState, pokemon.data]);
+  }
+
+  return (
     <div>
-    <div className="App">
-    <img src={Pokedex_logo} alt="pokedex-logo" className="pokedex-logo" />
-    <div className="search-container"> </div>
-    <CardList pokemon={pokemonArray}/>
-  </div>
-  <button onClick={handlePrevious}>PREVIOUS</button>
-  <button onClick={handleNext}>NEXT</button>
+      <div className="App">
+        <img src={Pokedex_logo} alt="pokedex-logo" className="pokedex-logo" />
+        <div className="search-container"> </div>
+        <CardList pokemon={pokeDetails} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
-
-// import React from "react";
-// import {
-//   Grid,
-//   CircularProgress,
-//   GridList,
-//   GridListTile,
-//   GridListTileBar
-// } from "@material-ui/core";
-// // import { Alert } from "@material-ui/lab";
-// import { useQuery } from "react-query";
-
-// const getPokemons = async () => {
-//   const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100");
-//   const data = await response.json();
-
-//   return data;
-// };
-
-// const getPokemon = async (url) => {
-//   const response = await fetch(url);
-//   const data = await response.json();
-
-//   return data;
-// };
-
-// const PokemonTile = ({ name, url }) => {
-//   const { error, isLoading, data } = useQuery(`pokemon${name}`, () =>
-//     getPokemon(url)
-//   );
-
-//   // if (error) {
-//   //   return <Alert severity="error">{error}</Alert>;
-//   // }
-
-//   if (isLoading) {
-//     return (
-//       <Grid container justify="center">
-//         <CircularProgress />
-//       </Grid>
-//     );
-//   }
-
-//   const {
-//     sprites: { front_default }
-//   } = data;
-
-//   return (
-//     <GridListTile>
-//       <img src={front_default} alt={name} />
-//       <GridListTileBar title={name} />
-//     </GridListTile>
-//   );
-// };
-
-// export const App = () => {
-//   const { error, isLoading, data } = useQuery("pokemons", getPokemons);
-
-//   if (error) {
-//     return <Alert severity="error">{error}</Alert>;
-//   }
-
-//   if (isLoading) {
-//     return (
-//       <Grid container justify="center">
-//         <CircularProgress />
-//       </Grid>
-//     );
-//   }
-
-//   const { results: pokemons } = data;
-
-//   return (
-//     <GridList cellHeight={300}>
-//       {pokemons.map((pokemon) => (
-//         <PokemonTile key={pokemon.name} {...pokemon} />
-//       ))}
-//     </GridList>
-//   );
-// };
-
-
-
-
-// // class App extends Component {
-// //   constructor() {
-// //     super();
-// //     this.state = {
-// //       pokemon: [],
-// //       nextUrl: "",
-// //       prevUrl: "",
-// //     };
-// //   }
-
-//   // componentDidMount() {
-//   //   const baseUrl = "https://pokeapi.co/api/v2/pokemon/?limit=27";
-//   //   try {
-//   //     fetch(baseUrl)
-//   //       .then((response) => {
-//   //         const responseJson = response.json();
-
-//   //         return responseJson;
-//   //       })
-//   //       .then(async (data) => {
-//   //         this.setState({ nextUrl: data.next });
-//   //         this.setState({ prevUrl: data.prev });
-
-//           // const pokemons = data.results;
-//           // for (const pokemon of pokemons) {
-//           //   pokemon.data = await fetch(pokemon.url).then((res) => res.json());
-//           // }
-//           // this.setState(
-//           //   () => {
-//           //     return { pokemon: pokemons };
-//           //   },
-//           //   () => {}
-//           // );
-//   //         window.onscroll = () => {
-//   //           if (
-//   //             window.innerHeight + document.documentElement.scrollTop ===
-//   //             document.documentElement.offsetHeight
-//   //           ) {
-//   //             this.fetchAPI(this.state.nextUrl);
-//   //           }
-//   //         };
-//   //       })
-//   //       .catch((error) => {
-//   //         console.error(error);
-//   //       });
-//   //   } catch (error) {
-//   //     console.error(error);
-//   //   }
-//   // }
-
-// //   render() {
-// //     return (
-//       // <div className="App">
-//       //   <img src={Pokedex_logo} alt="pokedex-logo" className="pokedex-logo" />
-//       //   <div className="search-container"> </div>
-//       //   <CardList pokemon={this.state.pokemon} />
-//       // </div>
-// //     );
-// //   }
-
-// //   fetchAPI(url) {
-// //     const baseUrl = url;
-// //     try {
-// //       fetch(baseUrl)
-// //         .then((response) => {
-// //           const responseJson = response.json();
-
-// //           return responseJson;
-// //         })
-// //         .then(async (data) => {
-// //           this.setState({ nextUrl: data.next });
-// //           this.setState({ prevUrl: data.prev });
-
-// //           const pokemons = data.results;
-// //           for (const pokemon of pokemons) {
-// //             pokemon.data = await fetch(pokemon.url).then((res) => res.json());
-// //           }
-
-// //           this.setState({
-// //             pokemon: this.state.pokemon.concat(pokemons),
-// //           });
-// //         })
-// //         .catch((error) => {
-// //           console.error(error);
-// //         });
-// //     } catch (error) {
-// //       console.error(error);
-// //     }
-// //   }
-// // }
-
-// // export default App;
-
-
+export default App;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,25 +1,21 @@
-import React, { Component } from "react";
 import "./card.styles.css";
 
-class Card extends Component {
-  render() {
-    const { name, id, height, weight, sprite, type1 } = this.props;
-
-    return (
-      <div className="card-container" key={id}>
-        <div className="img-wrapper">
-          <img className="sprite" src={sprite} alt={name} />
-        </div>
-        <h2 className="poke-name">
-          #{id} {name}
-        </h2>
-        <h4 className="height-weight">
-          {height}' - {weight} lbs
-        </h4>
-        <h3 className="type1">{type1}</h3>
+const Card = (props) => {
+  const { name, id, height, weight, sprite, type1, type2 } = props;
+  return (
+    <div className="card-container" key={id}>
+      <div className="img-wrapper">
+        <img className="sprite" src={sprite} alt={name} />
       </div>
-    );
-  }
-}
+      <h2 className="poke-name">
+        #{id} {name}
+      </h2>
+      <h4 className="height-weight">
+        {height} ft | {weight} lbs
+      </h4>
+      <h3 className="types">{type1}  {type2 ? `| ${type2}` : null }</h3>
+    </div>
+  );
+};
 
 export default Card;
